9d17161fca3d48005e5629899d5c4715ac853f32
Carlos Jaime <jaime_andrek@hotmail.com>
2023-01-19 09:54:33 -0300

    ajustes gerais

--- src/index.py
+++ src/index.py
@@ -12,30 +12,29 @@ class Count:
     def main(self):
         window = ThemedTk(theme='Black')
         window.title('Count+-')
-        window.resizable(0, 0)
-        window.geometry("300x400+500+25")
+        window.geometry("1200x720+100+0")
         window.iconbitmap("icon/logoF.ico")
         window.config(background="#ffffff")
 
         countFrame = Frame(window, background="#ffffff")
-        countFrame.pack(fill="x",pady=25)
+        countFrame.pack(fill="x",pady=45)
         
-        nameLabel = Label(countFrame, text="Contador", font="Gadugi 10", background="#ffffff")
+        nameLabel = Label(countFrame, text="Contador", font="Gadugi 20", background="#ffffff")
         nameLabel.pack(pady=30)
 
-        numberLabel = Label(countFrame, text=Number.getRegister(self), font="Gadugi 40", background="#ffffff")
+        numberLabel = Label(countFrame, text=Number.getRegister(self), font="Gadugi 130", background="#ffffff")
         numberLabel.pack()
 
         buttonFrame = Frame(window, background="#ffffff" )
-        buttonFrame.pack(pady=55)
+        buttonFrame.pack(pady=85)
 
-        buttonAdd = Button(buttonFrame, command=lambda: threading.Thread(target=Number().add(numberLabel)).start(), text="Adicionar",bd=0, width=10, height=2, background="#0E5369", fg="#ffffff")
+        buttonAdd = Button(buttonFrame, command=lambda: threading.Thread(target=Number().add(numberLabel)).start(), text="Adicionar",bd=0, width=30, height=4, background="#0E5369", fg="#ffffff")
         buttonAdd.pack(side="left",padx=2)
 
-        buttonRemove = Button(buttonFrame, command=lambda: threading.Thread(target=Number().remove(numberLabel)).start(), text="Remover", bd=0, width=10, height=2, background="#CC5354", fg="#ffffff")
+        buttonRemove = Button(buttonFrame, command=lambda: threading.Thread(target=Number().remove(numberLabel)).start(), text="Remover", bd=0, width=30, height=4, background="#CC5354", fg="#ffffff")
         buttonRemove.pack(side="left",padx=2)
         
-        buttonZerar = Button(buttonFrame, command=lambda: threading.Thread(target=Number().returnDefault(numberLabel)).start() , text="Redefinir", bd=0, width=10, height=2, fg="black")
+        buttonZerar = Button(buttonFrame, command=lambda: threading.Thread(target=Number().returnDefault(numberLabel)).start() , text="Redefinir", bd=0, width=30, height=4, fg="black")
         buttonZerar.pack(side="left",padx=2)
 
         developerFrame = Frame(window, background="#ffffff")
